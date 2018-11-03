e7c6cd95693eb09cee305dcdbcc844bf21c77924
Dominik Hardtke <dominik@hardtke.me>
2018-11-03 15:27:42 +0100

    fix: Use more permissive routing function to allow certain special chars (#15)
    * fix: Use more permissive routing function to allow certain special chars
    
    * Adjust pattern: Don't allow quotes
    
    * chore: Update tests for master

--- src/test/java/jdbi_modules/base/TestStructuredSqlGenerator.java
+++ src/test/java/jdbi_modules/base/TestStructuredSqlGenerator.java
@@ -3,15 +3,19 @@ package jdbi_modules.base;
 import jdbi_modules.internal.PrefixGenerator;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @since 14.04.2018
  */
-public class TestStructuredSqlGenerator {
+class TestStructuredSqlGenerator {
 
     @Test
     void testAppend() {
@@ -23,6 +27,23 @@ public class TestStructuredSqlGenerator {
                 new Full().sql(new HashSet<>(), new PrefixGenerator(""), Set.of(new Binding("lu", 12)), new Stack<>()), new Stack<>());
         new Full().append(new HashSet<>(), new PrefixGenerator(""), Set.of(),
                 new None().sql(new HashSet<>(), new PrefixGenerator(""), Set.of(), new Stack<>()), new Stack<>());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"\"user\"", ""})
+    void testRoutingRejecting(final String value) {
+        final Stack<String> stack = new Stack<>();
+        stack.push("mod0");
+        assertEquals("{{" + value + "}}", new DynamicSortOrderGenerator(value).sql(new HashSet<>(), new PrefixGenerator(""), Set.of(), stack).sortOrder);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"id", "mod0exercise.name->>'de'", "exercise.id", "exercise_id", "ÄÖÜßäöü", "_", "-", "0123456789", "a<b", "created - updated",
+            "function()"})
+    void testRoutingSuccessful(final String value) {
+        final Stack<String> stack = new Stack<>();
+        stack.push("mod0");
+        assertEquals("\"mod0" + value + "\"", new DynamicSortOrderGenerator(value).sql(new HashSet<>(), new PrefixGenerator(""), Set.of(), stack).sortOrder);
     }
 
     private class None implements StructuredSqlGenerator {
@@ -86,6 +107,20 @@ public class TestStructuredSqlGenerator {
         @Override
         public String filter() {
             return "A";
+        }
+    }
+
+    private class DynamicSortOrderGenerator implements StructuredSqlGenerator {
+        private final String sortOrder;
+
+        private DynamicSortOrderGenerator(String sortOrder) {
+            this.sortOrder = sortOrder;
+        }
+
+        @NotNull
+        @Override
+        public String sortOrder() {
+            return "{{" + this.sortOrder + "}}";
         }
     }
 }
